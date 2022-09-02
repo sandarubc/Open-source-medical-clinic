6b84747a5daa2876a39babd86e2683c1a54430af
Akalanka <sandaru.bc@gmail.com>
2022-09-02 13:16:38 +0530

    Close #2

--- src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
@@ -1,4 +1,14 @@
 package lk.ijse.dep9.clinic.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public JFXTextField txtUserName;
+    public JFXTextField txtPassword;
+    public JFXButton btnLogin;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
