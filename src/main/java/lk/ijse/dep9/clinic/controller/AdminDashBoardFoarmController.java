12edfd9672384bdf320a92c929500a48139de679
Akalanka <sandaru.bc@gmail.com>
2022-09-05 16:57:37 +0530

    Close #7

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashBoardFoarmController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashBoardFoarmController.java
@@ -1,9 +1,24 @@
 package lk.ijse.dep9.clinic.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import lk.ijse.dep9.clinic.security.SecurityContextHolder;
 
 public class AdminDashBoardFoarmController {
+    public JFXButton btnProfileManagment;
+    public JFXButton btnViewRecords;
+    public JFXButton btnSettings;
+
     public void initialize(){
         System.out.println(SecurityContextHolder.user.getUserName());
+    }
+
+    public void btnProfileManagmentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingsOnAction(ActionEvent actionEvent) {
     }
 }
